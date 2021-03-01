b49af81cff2614354e557d25117add2f6a961206
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-03-01 15:57:22 +0100

    keyboard awareness

--- App/src/Screens/Public/Registrieren.js
+++ App/src/Screens/Public/Registrieren.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View,TextInput,TouchableOpacity,KeyboardAvoidingView,ScrollView } from "react-native";
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default function Registrieren({navigation}){
     const styles = StyleSheet.create({
@@ -61,32 +62,32 @@ export default function Registrieren({navigation}){
         }
     });
     return(
-            <View style={styles.container}>
-                <View>
-                    <Text style={styles.title}>Go Fitness</Text>
-                </View>
-                <View>
-                    <Text style={styles.greet}>Fill in your Data</Text>
-                    <Text style={styles.describe}>Full Name</Text>
-                    <TextInput placeholder="Name" style={styles.input}/>
-                    <Text style={styles.describe}>Email</Text>
-                    <TextInput placeholder="Email" style={styles.input}/>
-                    <Text style={styles.describe}>Username</Text>  
-                    <TextInput placeholder="Username" style={styles.input}/>
-                    <Text style={styles.describe}>Password</Text>
-                    <TextInput placeholder="Password" style={styles.input} secureTextEntry={true}/>  
-                    <Text style={styles.describe}>Confirm Password</Text>
-                    <TextInput placeholder="Confirm Password" style={styles.input} secureTextEntry={true}/>  
-                </View>
-                <View style={styles.btnContainer}>
-                    <TouchableOpacity style={styles.register}>
-                        <Text style={styles.whiteText}>Register</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.signUpContainer}>
-                        <Text>Already have an Accound </Text>
-                        <Text style={styles.signUp}>Login here</Text>
-                    </TouchableOpacity>
-                </View>
+        <KeyboardAwareScrollView contentContainerStyle={styles.container} bounces={false}>
+            <View>
+                <Text style={styles.title}>Go Fitness</Text>
             </View>
+            <View>
+                <Text style={styles.greet}>Fill in your Data</Text>
+                <Text style={styles.describe}>Full Name</Text>
+                <TextInput placeholder="Name" style={styles.input}/>
+                <Text style={styles.describe}>Email</Text>
+                <TextInput placeholder="Email" style={styles.input}/>
+                <Text style={styles.describe}>Username</Text>  
+                <TextInput placeholder="Username" style={styles.input}/>
+                <Text style={styles.describe}>Password</Text>
+                <TextInput placeholder="Password" style={styles.input} secureTextEntry={true}/>  
+                <Text style={styles.describe}>Confirm Password</Text>
+                <TextInput placeholder="Confirm Password" style={styles.input} secureTextEntry={true}/>  
+            </View>
+            <View style={styles.btnContainer}>
+                <TouchableOpacity style={styles.register}>
+                    <Text style={styles.whiteText}>Register</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.signUpContainer}>
+                    <Text>Already have an Accound </Text>
+                    <Text style={styles.signUp}>Login here</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAwareScrollView>
     )
 }
