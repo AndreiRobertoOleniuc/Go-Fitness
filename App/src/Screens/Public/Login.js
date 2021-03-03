9b8f0e44938bde2ee592b990aa6037ba400ad3a4
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-03-03 07:35:05 +0100

    up to date

--- App/src/Screens/Public/Login.js
+++ App/src/Screens/Public/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Button,TextInput,TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from "react-native";
 
 export default function Login({navigation}){
     return(
