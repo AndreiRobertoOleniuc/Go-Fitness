75bf2599c92f9a001996bbd541d73156e9bf31e3
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-03-10 13:22:28 +0100

    real shit starts

---
+++ App/src/Screens/Private/Screen/Tagebuch.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View,Text,TouchableOpacity,StyleSheet,ScrollView} from "react-native";
+
+export default function Tagebuch() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>tagebuhc!</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container:{
+
+    },
+});
