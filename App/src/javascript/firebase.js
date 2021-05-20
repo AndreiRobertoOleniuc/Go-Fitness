ddfb2b801406ef97c5ce226cdee07a9b723bb71f
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-05-20 10:52:06 +0200

    firebase project replaced

--- App/src/javascript/firebase.js
+++ App/src/javascript/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKj3fRaNw3v7QsoEZgL5mCrTGG72Aii3Q",
-    authDomain: "gofitme-20335.firebaseapp.com",
-    projectId: "gofitme-20335",
-    storageBucket: "gofitme-20335.appspot.com",
-    messagingSenderId: "712061508841",
-    appId: "1:712061508841:web:f5c4603cd093f010a92478"
+    apiKey: "AIzaSyApqSLy-9vzM5KI8kF6qcoHd7OjVbuPFHs",
+    authDomain: "gofit-f423b.firebaseapp.com",
+    projectId: "gofit-f423b",
+    storageBucket: "gofit-f423b.appspot.com",
+    messagingSenderId: "179650405725",
+    appId: "1:179650405725:web:98a6517f0f1714256574b4"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
